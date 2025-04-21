afa2240fe9e69aeb05b8f951a6163adbe885aa3d
Max-Pang-Real <mapang@s.sfusd.edu>
2025-04-21 12:12:13 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,18 @@
 public class APLine
 {
-  /* your code here */
-  
+  private int myA, myB, myC;
+  public APLine(int a, int b, int c){
+    myA = a;
+    myB = b;
+    myC = c;
+  }
+  public double getSlope(){
+    return -myA/myB;
+  }
+  public boolean isOnLine(int x, int y){
+    if((myA*x) + (myB*y) + myC == 0){
+      return true;
+    }
+    return false;
+  }
 }
